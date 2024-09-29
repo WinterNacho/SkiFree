7a6deec1516c285a345c3b33811cf8104743264d
WinterNacho <winternacho@gmail.com>
2024-09-29 19:30:48 -0300

    Update globals

--- source/globals.cpp
+++ source/globals.cpp
@@ -1,9 +1,9 @@
 #include "globals.h"
 
-float randomPosition(){
+float randomPosition(float min_float, float max_float){
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> distrib(-SCREEN_SIZE_X * 0.125f, SCREEN_SIZE_X * 1.125f);
+    std::uniform_real_distribution<float> distrib(min_float, max_float);
 
     return distrib(gen);
 }
--- source/globals.h
+++ source/globals.h
@@ -6,7 +6,7 @@
 const int SCREEN_SIZE_X = 640;
 const int SCREEN_SIZE_Y = 400;
 
-float randomPosition();
+float randomPosition(float min_float=-SCREEN_SIZE_X * 0.125f, float max_float = SCREEN_SIZE_X * 1.125f );
 
 
 #endif // GLOBALS_H
