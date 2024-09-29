c09ef21fd33a048a28ffd2298e9ea90b710cc443
WinterNacho <winternacho@gmail.com>
2024-09-29 19:31:25 -0300

    Add Textures

---
+++ source/textures/textures.h
@@ -0,0 +1,12 @@
+#ifndef TEXTURES_H
+#define TEXTURES_H
+#include <nothofagus.h>
+
+std::vector <Nothofagus::Texture> setupPlayerTextures();
+//Nothofagus::Texture setupPlayerTextures();
+Nothofagus::Texture setupHydrantTextures();
+Nothofagus::Texture setupTreeTextures();
+Nothofagus::Texture setupRampTextures();
+Nothofagus::Texture setupRampTextures();
+Nothofagus::Texture setupMonster();
+#endif 
